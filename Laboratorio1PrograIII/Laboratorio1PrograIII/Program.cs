9178af0a8ea0fa7ca57b004b8b482f0477c44a22
Marie01 <87671658+maryrivas01@users.noreply.github.com>
2023-02-22 15:02:48 -0600

    lab1

---
+++ Laboratorio1PrograIII/Laboratorio1PrograIII/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1PrograIII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+           public class Persona
+        {
+            private string nombre;
+            private int edad;
+
+            public string Nombre
+            {
+                get => nombre;
+                set => nombre = value;
+            }
+
+            public int Edad
+            {
+                get => edad;
+                set => edad = value;
+            }
+        }
+
+    }
+    }
+}    
